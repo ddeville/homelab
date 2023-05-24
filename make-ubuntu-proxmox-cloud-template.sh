666c76e7910b9e260452fdbda95845fd00255293
Damien Deville <damien@ddeville.me>
2023-05-23 17:58:11 -0700

    shellcheck

--- make-ubuntu-proxmox-cloud-template.sh
+++ make-ubuntu-proxmox-cloud-template.sh
@@ -7,13 +7,13 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-SCRIPT_DIR="$( dirname -- "$BASH_SOURCE"; )";
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 UBUNTU_VERSION="${UBUNTU_VERSION:-jammy}"
 VM_ID="${VM_ID:-8000}"
 
 WORK_DIR="$(mktemp -d)"
-trap "rm -f ${WORK_DIR}/${UBUNTU_VERSION}-server-cloudimg-amd64.img" EXIT
+trap 'rm -f "${WORK_DIR}/${UBUNTU_VERSION}-server-cloudimg-amd64.img"' EXIT
 
 PATH=/usr/sbin/:$PATH
 
@@ -21,18 +21,18 @@ pushd "${WORK_DIR}"
 
 wget "https://cloud-images.ubuntu.com/${UBUNTU_VERSION}/current/${UBUNTU_VERSION}-server-cloudimg-amd64.img"
 
-qm create ${VM_ID} --memory 2048 --core 2 --name ${UBUNTU_VERSION}-cloud --net0 virtio,bridge=vmbr0,firewall=1,trunks=40
+qm create "${VM_ID}" --memory 2048 --core 2 --name "${UBUNTU_VERSION}-cloud" --net0 virtio,bridge=vmbr0,firewall=1,trunks=40
 
-qm importdisk ${VM_ID} "${UBUNTU_VERSION}-server-cloudimg-amd64.img" data-lvm
-qm set ${VM_ID} --scsihw virtio-scsi-pci --scsi0 data-lvm:vm-${VM_ID}-disk-0
-qm set ${VM_ID} --boot c --bootdisk scsi0
-qm resize ${VM_ID} scsi0 +16G
-qm set ${VM_ID} --ide2 data-lvm:cloudinit
-qm set ${VM_ID} --serial0 socket --vga serial0
-qm set ${VM_ID} --sshkey "${SCRIPT_DIR}/id_rsa.pub"
-qm set ${VM_ID} --ciuser "damien"
-qm set ${VM_ID} --ipconfig0 ip=dhcp
+qm importdisk "${VM_ID}" "${UBUNTU_VERSION}-server-cloudimg-amd64.img" data-lvm
+qm set "${VM_ID}" --scsihw virtio-scsi-pci --scsi0 data-lvm:vm-"${VM_ID}"-disk-0
+qm set "${VM_ID}" --boot c --bootdisk scsi0
+qm resize "${VM_ID}" scsi0 +16G
+qm set "${VM_ID}" --ide2 data-lvm:cloudinit
+qm set "${VM_ID}" --serial0 socket --vga serial0
+qm set "${VM_ID}" --sshkey "${SCRIPT_DIR}/id_rsa.pub"
+qm set "${VM_ID}" --ciuser "damien"
+qm set "${VM_ID}" --ipconfig0 ip=dhcp
 
-qm template ${VM_ID}
+qm template "${VM_ID}"
 
 popd
